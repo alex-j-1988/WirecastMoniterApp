978dae8676a323e8d68660c7462c82b29ad455c1
Alex Jiang <44747687+alex-j-1988@users.noreply.github.com>
2018-12-18 21:19:35 +0300

    Firt commit
    Commit

---
+++ WirecastMoniterApp/frmMain.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WirecastMoniterApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+
+            timer1.Stop();
+            _Wirecast = null;            
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStartMonitoring = new System.Windows.Forms.Button();
+            this.cbInterval = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStartMonitoring
+            // 
+            this.btnStartMonitoring.Location = new System.Drawing.Point(14, 12);
+            this.btnStartMonitoring.Name = "btnStartMonitoring";
+            this.btnStartMonitoring.Size = new System.Drawing.Size(142, 23);
+            this.btnStartMonitoring.TabIndex = 2;
+            this.btnStartMonitoring.Text = "Start Monitoring";
+            this.btnStartMonitoring.UseVisualStyleBackColor = true;
+            this.btnStartMonitoring.Click += new System.EventHandler(this.btnStartMonitoring_Click);
+            // 
+            // cbInterval
+            // 
+            this.cbInterval.FormattingEnabled = true;
+            this.cbInterval.Items.AddRange(new object[] {
+            "1",
+            "3",
+            "5",
+            "10",
+            "20",
+            "30"});
+            this.cbInterval.Location = new System.Drawing.Point(60, 48);
+            this.cbInterval.Name = "cbInterval";
+            this.cbInterval.Size = new System.Drawing.Size(66, 21);
+            this.cbInterval.TabIndex = 3;
+            this.cbInterval.Text = "1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Interval";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Min";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(171, 93);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbInterval);
+            this.Controls.Add(this.btnStartMonitoring);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wirecast monitor";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStartMonitoring;
+        private System.Windows.Forms.ComboBox cbInterval;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WirecastMoniterApp/frmMain.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+
+namespace WirecastMoniterApp
+{
+    public partial class frmMain : Form
+    {
+        private WirecastBinding _Wirecast;
+        private bool _IsStarted = false;
+        private string _Logfile = @"wirecast_start.log";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            _Wirecast = new WirecastBinding();
+            _Wirecast.Initialize();
+        }
+
+        private void btnStartMonitoring_Click(object sender, EventArgs e)
+        {
+            if (!_IsStarted)
+            {
+                int interval = int.Parse(cbInterval.Text);
+
+                timer1.Interval = interval * 1000 * 60;
+                timer1.Start();
+                cbInterval.Enabled = false;
+                _IsStarted = true;
+                btnStartMonitoring.Text = @"Stop monitoring.";
+
+                LogWrite("Start monitoring...");
+            }
+            else
+            {
+                timer1.Stop();
+                cbInterval.Enabled = true;
+                btnStartMonitoring.Text = @"Start monitoring.";
+                _IsStarted = false;
+
+                LogWrite("Stop monitoring");
+            }
+        }
+
+        public void LogWrite(string logMessage)
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            try
+            {
+                using (StreamWriter w = File.AppendText(path + "\\" + _Logfile))
+                {
+                    Log(logMessage, w);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public void Log(string logMessage, TextWriter txtWriter)
+        {
+            try
+            {
+                txtWriter.WriteLine("-------------------------------");
+                txtWriter.WriteLine(DateTime.Now.ToString() + " : " + logMessage);
+                txtWriter.WriteLine("-------------------------------");
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!_Wirecast.IsBroadcasting())
+            {
+                LogWrite("Wirecast is stopped. Start now...");
+                var notifier = new PopupNotifier()
+                {
+                    TitleText = @"Wirecast broadcast is stopped!",
+                    TitleFont = new Font("Times New Roman", 20.0f),
+                    ContentText = @"Start broadcast again!",
+                    ContentFont = new Font("Times New Roman", 17.0f),
+                };
+                notifier.Popup();
+
+
+                _Wirecast.StartBroadcast();
+            }
+        }
+    }
+}
